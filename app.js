2088cc03d03c5afe87c9c481cf60d859867dab21
郝钰 <haoyu@csdn.net>
2021-10-28 15:03:10 +0800

    配置Prettier格式化代码

--- app.js
+++ app.js
@@ -1,50 +1,49 @@
 const express = require('express');
-const {createProxyMiddleware} = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const ipaddr = require('ipaddr.js');
 
-const HOST= process.env.HOST?process.env.HOST:'localhost'
-const PORT= process.env.PORT?process.env.PORT:3000
+const HOST = process.env.HOST ? process.env.HOST : 'localhost';
+const PORT = process.env.PORT ? process.env.PORT : 3000;
 const app = express();
 
 var parseUrl = function (path) {
-  let ps = path.split('/')
-  if (ps.length <= 3 )
-    return null;
-  let host = ps[2]
-  for (let i = 0;i<3;i++) ps.shift()
-  return {'host':host, 'path':'/' + ps.join('/')}
-}
+  let ps = path.split('/');
+  if (ps.length <= 3) return null;
+  let host = ps[2];
+  for (let i = 0; i < 3; i++) ps.shift();
+  return { host: host, path: '/' + ps.join('/') };
+};
 const options = {
   target: `http://${HOST}:${PORT}`,
   changeOrigin: true,
   router: function (req) {
-    let newPath = parseUrl(req.originalUrl) 
-    if(!newPath) return ''
-    let host = newPath.host.split(':')[0]
-    if(!ipaddr.isValid(host)) {
-      console.log('无效的主机：' + host)
-      return ''
+    let newPath = parseUrl(req.originalUrl);
+    if (!newPath) return '';
+    let host = newPath.host.split(':')[0];
+    if (!ipaddr.isValid(host)) {
+      console.log('无效的主机：' + host);
+      return '';
     }
-    if(ipaddr.parse(host).range()=='private') {
-      console.log('无效的主机：' + host)
-      return ''
+    if (ipaddr.parse(host).range() == 'private') {
+      console.log('无效的主机：' + host);
+      return '';
     }
-    return newPath?`http://${newPath.host}`:''
+    return newPath ? `http://${newPath.host}` : '';
   },
-  pathRewrite: function (path, req) { 
-    newPath = parseUrl(path)
-    return newPath?`http://${newPath.path}`:''
-  }
-}
+  pathRewrite: function (path, req) {
+    newPath = parseUrl(path);
+    return newPath ? `http://${newPath.path}` : '';
+  },
+};
 
 const proxyServer = createProxyMiddleware(options);
 
 app.use('/431387eb7262e1cfc79b125eb8a67c60', proxyServer);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
-app.listen(PORT, HOST,() => {
-    console.log(`Starting Proxy at ${HOST}:${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Starting Proxy at ${HOST}:${PORT}`);
 });
